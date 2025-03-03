ae2aeff28c689f3d15aa28ff98f8343cb191cb40
Kirius257 <holin.kirill@yandex.ru>
2025-03-03 16:26:16 +0300

    fix

--- tasks/omp/kholin_k_multidimensional_integrals_rectangle/func_tests/main.cpp
+++ tasks/omp/kholin_k_multidimensional_integrals_rectangle/func_tests/main.cpp
@@ -404,46 +404,6 @@ TEST(kholin_k_multidimensional_integrals_rectangle_omp, triple_integral_one_var)
   ASSERT_EQ(ref_i, std::round(out_i[0]));
 }
 
-//TEST(kholin_k_multidimensional_integrals_rectangle_omp, triple_integral_three_var_high_acc) {
-//  // Create data
-//  size_t dim = 3;
-//  std::vector<double> values{0.0, 0.0, 0.0};
-//  auto f = [](const std::vector<double> &f_values) {
-//    return (1.0 / 9.0) * (f_values[0] * f_values[1] * f_values[2]);
-//  };
-//  std::vector<double> in_lower_limits{-1, -1, -1};
-//  std::vector<double> in_upper_limits{1, 1, 1};
-//  double n = 150.0;
-//  std::vector<double> out_i(1, 0.0);
-//
-//  auto f_object = std::make_unique<std::function<double(const std::vector<double> &)>>(f);
-//
-//  // Create task_data
-//  std::shared_ptr<ppc::core::TaskData> task_data_omp = std::make_shared<ppc::core::TaskData>();
-//  task_data_omp->inputs.emplace_back(reinterpret_cast<uint8_t *>(&dim));
-//  task_data_omp->inputs.emplace_back(reinterpret_cast<uint8_t *>(values.data()));
-//  task_data_omp->inputs.emplace_back(reinterpret_cast<uint8_t *>(f_object.get()));
-//  task_data_omp->inputs.emplace_back(reinterpret_cast<uint8_t *>(in_lower_limits.data()));
-//  task_data_omp->inputs.emplace_back(reinterpret_cast<uint8_t *>(in_upper_limits.data()));
-//  task_data_omp->inputs.emplace_back(reinterpret_cast<uint8_t *>(&n));
-//  task_data_omp->inputs_count.emplace_back(values.size());
-//  task_data_omp->inputs_count.emplace_back(in_lower_limits.size());
-//  task_data_omp->inputs_count.emplace_back(in_upper_limits.size());
-//  task_data_omp->outputs.emplace_back(reinterpret_cast<uint8_t *>(out_i.data()));
-//  task_data_omp->outputs_count.emplace_back(out_i.size());
-//
-//  // Create Task
-//  kholin_k_multidimensional_integrals_rectangle_omp::TestTaskOpenMP test_task_omp(task_data_omp);
-//  ASSERT_EQ(test_task_omp.Validation(), true);
-//  ASSERT_EQ(test_task_omp.PreProcessing(), true);
-//  ASSERT_EQ(test_task_omp.Run(), true);
-//  ASSERT_EQ(test_task_omp.PostProcessing(), true);
-//
-//  double ref_i = 0.888888;
-//  std::cout << "       " << out_i[0] << '\n';
-//  ASSERT_NEAR(ref_i, out_i[0], 1e-3);
-//}
-
 TEST(kholin_k_multidimensional_integrals_rectangle_omp, double_integral_two_var_high_acc) {
   // Create data
   size_t dim = 2;
